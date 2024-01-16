613694717ecfcfba249f8488aa776240dd7ed4e5
Luís Filipe Barbosa <lfb@utad.pt>
2024-01-16 10:52:26 +0000

    Resolução completa

---
+++ PAPP2/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using PAPP2.Data;
+using PAPP2.Models;
+using System.Diagnostics;
+
+namespace PAPP2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UnidadesCurriculares
+        public async Task<IActionResult> Index()
+        {
+            return _context.UnidadesCurriculares != null ?
+                        View(await _context.UnidadesCurriculares.Include(i => i.Inscricoes.Where(x=>x.Ano == DateTime.Now.Year)).ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.UnidadesCurriculares'  is null.");
+        }
+
+        [Authorize]
+        public async Task<int> Inscreve(string id)
+        {
+            Inscricao nova = new Inscricao();
+            nova.Ano = DateTime.Now.Year;
+            nova.Username = User.Identity.Name;
+            nova.UCId = id;
+
+            _context.Inscricoes.Add(nova);
+
+            await _context.SaveChangesAsync();
+
+            int contagem = _context.Inscricoes.Where(i => i.UCId == id && i.Ano == DateTime.Now.Year).Count();
+
+            return contagem;
+        }
+
+        [Authorize]
+        public async Task<int> Desinscreve(string id)
+        {
+            Inscricao existente= _context.Inscricoes.SingleOrDefault(i => i.UCId == id && 
+                                                            i.Ano== DateTime.Now.Year && i.Username == User.Identity.Name);
+            if(existente != null)
+            {
+                _context.Inscricoes.Remove(existente);
+
+                await _context.SaveChangesAsync();
+            }
+
+            // interessa a contagem das inscrições desta UC neste ano civil (independentemente do utilizador)
+            int contagem = _context.Inscricoes.Where(i => i.UCId == id && i.Ano == DateTime.Now.Year).Count();
+
+            return contagem;
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PAPP2/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PAPP2.Models;
+
+namespace PAPP2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UnidadeCurricular> UnidadesCurriculares { get; set; }
+
+        public DbSet<Inscricao> Inscricoes { get; set; }
+    }
+}
